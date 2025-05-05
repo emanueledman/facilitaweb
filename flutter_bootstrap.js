8c8cb6afada1b1ebe625e829b83d7d69a8b701af
Emanuel Joao <emanuelhedima@gmail.com>
2025-05-05 10:20:56 +0100

    ghjyg

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "38129972"
+    serviceWorkerVersion: "3710818857"
   }
 });
