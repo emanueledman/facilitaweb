d006cef5e48e54de20186a82c3646d757390112d
Emanuel Joao <emanuelhedima@gmail.com>
2025-06-22 20:03:04 +0100

    gfhjdg

--- auth.js
+++ auth.js
@@ -1,12 +1,4 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDVtY6ML3j-qrIsAprIJPB5xFFCbcf4UQw",
-  authDomain: "facilita-479b3.firebaseapp.com",
-  databaseURL: "https://facilita-479b3-default-rtdb.firebaseio.com",
-  projectId: "facilita-479b3",
-  storageBucket: "facilita-479b3.appspot.com",
-  messagingSenderId: "385676676886",
-  appId: "1:385676676886:web:6976de7f3abc6c0da94a37"
-};
+
 
 const firebaseAuth = {
   _app: null,
